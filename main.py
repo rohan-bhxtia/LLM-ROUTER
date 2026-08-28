40ce86b1c107d4542e4a1143a71357ffb7002ad3
Rohan <rohanbhatia003@gmail.com>
2026-08-28 10:45:03 +0530

    minor changes

--- main.py
+++ main.py
@@ -2,11 +2,21 @@ import os
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from pydantic import BaseModel, Field
 
 load_dotenv()
 
 Api_Key = os.getenv("Gemini_Api")
 client = genai.Client(api_key= Api_Key)
+
+class Schema(BaseModel):
+    topic:str = Field(
+        description="name the topic"
+    )
+    fact:str = Field(
+        description="explain fact in 1 line"
+    )
+
 
 history = []
 def ask_ques(prompt)-> str:
@@ -21,14 +31,16 @@ def ask_ques(prompt)-> str:
             temperature=0.1,
             system_instruction="ruthless sarcastic",
             automatic_function_calling=types.AutomaticFunctionCallingConfig(
-            disable=True)
+            disable=True),
+            response_mime_type="application/json",
+            response_schema= Schema
         )
     )
-    
-    full_text = response.text
+
+    full_text = response.parsed
     history.append(types.Content(
         role="model",
-        parts=[types.Part(text=full_text)]
+        parts=[types.Part(text=response.text)]
     )
 )    
     return full_text
@@ -37,9 +49,12 @@ def ask_ques(prompt)-> str:
 
 
 if __name__ == "__main__":
-    while True:
 
+    while True:
         prmpt = input("ask ur ques mf: ")
         if prmpt.lower() == "quit":
             break
-        print("model: ", ask_ques(prmpt))
+        result = ask_ques(prmpt)
+
+        print("Topic: ", result.topic)
+        print("Fact: ", result.fact)
