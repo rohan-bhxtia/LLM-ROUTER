6bca82dc38b28ccbcd7b49a8ef465bc5f96d4f66
Rohan <rohanbhatia003@gmail.com>
2026-08-30 19:16:51 +0530

    error handling added

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import os
 from dotenv import load_dotenv
 from google import genai
-from google.genai import types
+from google.genai import types , errors
 from pydantic import BaseModel, Field
+
 
 load_dotenv()
 
@@ -17,44 +18,48 @@ class Schema(BaseModel):
         description="explain fact in 1 line"
     )
 
-
 history = []
-def ask_ques(prompt)-> str:
+def ask_ques(prompt)-> Schema:
     history.append(types.Content(
         role="user",
         parts=[types.Part(text=prompt)]
     ))
-    response = client.models.generate_content(
-        model = "gemini-3.6-flash",
-        contents = history,
-        config = types.GenerateContentConfig(
-            temperature=0.1,
-            system_instruction="ruthless sarcastic",
-            automatic_function_calling=types.AutomaticFunctionCallingConfig(
-            disable=True),
-            response_mime_type="application/json",
-            response_schema= Schema
+    try:
+        response = client.models.generate_content(
+            model = "gemini-3.6-flash",
+            contents = history,
+            config = types.GenerateContentConfig(
+                temperature=0.1,
+                system_instruction="ruthless sarcastic abusive angry",
+                automatic_function_calling=types.AutomaticFunctionCallingConfig(
+                disable=True),
+                response_mime_type="application/json",
+                response_schema= Schema
         )
     )
+    except errors.ClientError as e:
+        print("code:",e.code)
+        print("Message:",e.message)    
+        return
 
     full_text = response.parsed
+    token_count = response.usage_metadata.total_token_count
     history.append(types.Content(
         role="model",
         parts=[types.Part(text=response.text)]
     )
 )    
-    return full_text
-
-
+    return full_text , token_count
 
 
 if __name__ == "__main__":
-
     while True:
         prmpt = input("ask ur ques mf: ")
         if prmpt.lower() == "quit":
             break
-        result = ask_ques(prmpt)
+        result, token_count = ask_ques(prmpt)
 
         print("Topic: ", result.topic)
         print("Fact: ", result.fact)
+        print("------USAGE-------")
+        print("total tokens: ", token_count)
